0976376f5d7333bb7f4d4124ac9cb0c450af181e
陈毅杰 <1121053165@qq.com>
2026-09-03 11:55:43 +0800

    fix:修复mysql问题

--- wx-front/config/api.js
+++ wx-front/config/api.js
@@ -1,7 +1,7 @@
 // const ApiRootUrl = 'https://01bcd133.ngrok.io/';
 // const WebSocktUrl = 'ws://f5763704.ngrok.io/'
-const ApiRootUrl = 'http://127.0.0.1:8080/';
-const WebSocktUrl = 'ws://127.0.0.1:8805/'
+const ApiRootUrl = 'http://159.75.222.113:8080/';
+const WebSocktUrl = 'ws://159.75.222.113:8080/'
 
 module.exports = {
   //首页
